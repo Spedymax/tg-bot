a664dea86a01c59d3f270003d91baf8471fe5c67
Spedymax <sokolov.maksim10@gmail.com>
2023-10-16 14:40:48 +0200

    Update 2.05 fix

---
+++ Sleep.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+
+while True:
+    # Move the mouse to the right
+    pyautogui.move(100, 0, duration=1)  # Adjust the distance (100) and duration (1) as needed
+
+    # Move the mouse to the left
+    pyautogui.move(-100, 0, duration=1)  # Adjust the distance (-100) and duration (1) as needed
+
+    # Wait for 3 minutes (180 seconds)
+    time.sleep(60)
