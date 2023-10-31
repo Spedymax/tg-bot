a9ac19ced03ee83a9b0b9800e0d23ab1310401cb
Spedymax <sokolov.maksim10@gmail.com>
2023-10-31 13:37:10 +0100

    Update 2.09 kazik fix 3

--- main.py
+++ main.py
@@ -1583,8 +1583,8 @@ def can_use_pisunchik():
                 if not pisunchik[player]['notified']:
                     if player != '1561630034':
                         player_name2 = get_player_name(player)
-                        # bot.send_message(-1001294162183, f"{player_name2} теперь может использовать /pisunchik")
-                        # pisunchik[player]['notified'] = True
+                        bot.send_message(-1001294162183, f"{player_name2} теперь может использовать /pisunchik")
+                        pisunchik[player]['notified'] = True
                         save_data()
         curr_time = datetime.now(timezone.utc) + timedelta(hours=2)
         if curr_time.hour == 12 and curr_time.minute == 0:
