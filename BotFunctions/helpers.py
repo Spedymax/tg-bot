d4a06f20626055b26f8de70186083c38ff8159b5
Maksym Sokolov <mso@anthillagency.com>
2024-09-05 18:29:07 +0200

    Update 4.1

--- BotFunctions/helpers.py
+++ BotFunctions/helpers.py
@@ -1,3 +1,6 @@
 
 def send_message_to_group(bot, message, parse_mode='html'):
     bot.send_message(-1001294162183, message, parse_mode)
+
+def send_message_to_group2(bot, message, parse_mode='html'):
+    bot.send_message(-4539972294, message, parse_mode)
