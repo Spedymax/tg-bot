888d83240a17a380bc4b0b3026dee1a434af0dbd
Maksym Sokolov <mso@anthillagency.com>
2024-09-10 17:03:57 +0200

    Update 4.2

--- main.py
+++ main.py
@@ -1791,10 +1791,10 @@ def can_use_pisunchik():
         # if curr_time.hour in [8, 13, 17] and curr_time.minute == 0:
         #     stocks.update_stock_prices(cursor, bot, helper)
         if curr_time.hour in [10, 15, 18] and curr_time.minute == 0:
-            for chat_id in [-1001294162183, -4539972294]:
+            for chat_id in [-1001294162183, -1002491624152]:
                 trivia.send_trivia_questions(chat_id, bot, cursor, conn, headers)
         if curr_time.hour == 21 and curr_time.minute == 50:
-            for chat_id in [-1001294162183, -4539972294]:
+            for chat_id in [-1001294162183, -1002491624152]:
                 trivia.get_correct_answers(bot, pisunchik, cursor, chat_id)
         for player in pisunchik:
             existing_characteristic = pisunchik[player]['characteristics']
