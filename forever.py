4380660e9884263eeea6cf7955b6fbeb63d23274
Spedymax <sokolov.maksim10@gmail.com>
2024-10-01 13:25:24 +0200

    Update 4.9

--- forever.py
+++ forever.py
@@ -3,10 +3,8 @@ import os
 from subprocess import Popen
 
 filename = "main.py"
-script_dir = os.path.dirname(os.path.abspath(__file__))  # Get script directory
 
 while True:
     print("\nStarting " + filename)
-    os.chdir(script_dir)  # Change directory to script location (tg-bot)
-    p = Popen("python " + filename, shell=True)
+    p = Popen("/home/spedymax/venv/bin/python3 " + filename, shell=True)
     p.wait()
