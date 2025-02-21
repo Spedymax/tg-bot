5764369ffe7e7b1ef92e2d8e69b805d0bde002c0
Spedymax <sokolov.maksim10@gmail.com>
2025-02-21 14:22:09 +0100

    Update 6.5

--- BotFunctions/cryptography.py
+++ BotFunctions/cryptography.py
@@ -1,6 +1,7 @@
 import Crypto
 from openai import OpenAI
 from openpyxl import load_workbook
+from google import genai
 
 encrypted_file = 'encrypted.xlsx'  # Replace with path for the encrypted file
 decrypted_file = 'decrypted.xlsx'  # Replace with path for the decrypted file
@@ -11,7 +12,12 @@ workbook = load_workbook(filename='decrypted.xlsx')
 sheet = workbook.active  # Assumes you want the active sheet
 
 # Read the value from cell A1
-api_key = sheet['A1'].value
+openai_api_key = sheet['A1'].value
+google_api_key = sheet['A2'].value
 client = OpenAI(
-    api_key=f'{api_key}'
+    api_key=f'{openai_api_key}'
+)
+
+clientGoogle = genai.Client(
+    api_key=f'{google_api_key}'
 )
