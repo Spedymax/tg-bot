df9ab9111763f3e864d34198c5be1df6a86faa65
Maksym Sokolov <mso@anthillagency.com>
2026-03-07 20:48:16 +0100

    feat: add FSM state groups for aiogram v3 migration

---
+++ src/states/__init__.py
@@ -0,0 +1,6 @@
+from .registration import RegistrationStates
+from .shop import ShopStates
+from .court import CourtStates
+from .danetka import DanetkaStates
+
+__all__ = ['RegistrationStates', 'ShopStates', 'CourtStates', 'DanetkaStates']
